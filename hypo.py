edd52d7ae8eb311ca078cbaeb28a73e12bc8c7d6
Ian Birnam <ian@makeschool.com>
2019-08-27 13:39:25 -0700

    added comment

--- hypo.py
+++ hypo.py
@@ -1,6 +1,7 @@
 side_a = 15
 side_b = 6
 
+# This function finds the hypotenuse of a triangle with the other two sides provided
 def calculateHypotenuse():
   side_c = sqrt(side_a * side_a + side_b * side_b)
   return side_c
